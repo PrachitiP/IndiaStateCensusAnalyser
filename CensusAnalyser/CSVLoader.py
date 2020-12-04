f5fa669294ed2b4e923c9b609a68345142335631
PrachitiP <patilprachiti35@gmail.com>
2020-12-04 16:31:14 +0530

    Load State Code

--- CensusAnalyser/CSVLoader.py
+++ CensusAnalyser/CSVLoader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from com.bridgelabz.CensusAnalyser.CensusAnalyserError import CensusAnalyserError
 from com.bridgelabz.CensusAnalyser.IndianCensus import IndianCensus
+from com.bridgelabz.CensusAnalyser.IndianCensus import IndianStateCode
 
 class CSVLoader:
     def __init__(self, path):
@@ -19,8 +20,25 @@ class CSVLoader:
         except FileNotFoundError:
             raise CensusAnalyserError("Check file path")
         except ValueError:
-            raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
+            raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name"
+
+    def load_State_Code(self):
+
+        try:
+            col_names = repr(IndianStateCode()).split(",")
+
+            data = pd.read_csv(self.path, usecols=col_names)
+            return len(data)
+
+        except FileNotFoundError:
+                raise CensusAnalyserError("Check file path")
+        except ValueError:
+                raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
+
+
 if __name__ == "__main__":
     x  = CSVLoader(path="C:/Users/PRACHITI PATIL/PycharmProjects/CensusAnalyzer/resources/IndiaStateCensusData.csv")
     length = x.record_counter()
     print(length)
+    len1 = x.load_State_Code()
+    print(len1)
