201bb2ff75972313776286dbff956a7b584a1db3
PrachitiP <patilprachiti35@gmail.com>
2020-12-04 17:07:34 +0530

    REFACTORED:Load CSV file

--- CensusAnalyser/CSVLoader.py
+++ CensusAnalyser/CSVLoader.py
@@ -2,10 +2,12 @@ import pandas as pd
 from com.bridgelabz.CensusAnalyser.CensusAnalyserError import CensusAnalyserError
 from com.bridgelabz.CensusAnalyser.IndianCensus import IndianCensus
 from com.bridgelabz.CensusAnalyser.IndianCensus import IndianStateCode
+from com.bridgelabz.CensusAnalyser.IndiaStateCode import StateCensusAnalyzer
 
 class CSVLoader:
-    def __init__(self, path):
+    def __init__(self, path, obj):
         self.path = path
+        self.obj = obj
 
     def record_counter(self):
         """
@@ -13,32 +15,20 @@ class CSVLoader:
         :return: number of records in file
         """
         try:
-            col_names = repr(IndianCensus()).split(",")
-            #data = pd.read_csv(self.path)
+            col_names = repr(self.obj).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
             return len(data)
         except FileNotFoundError:
             raise CensusAnalyserError("Check file path")
         except ValueError:
-            raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name"
-
-    def load_State_Code(self):
-
-        try:
-            col_names = repr(IndianStateCode()).split(",")
-
-            data = pd.read_csv(self.path, usecols=col_names)
-            return len(data)
-
-        except FileNotFoundError:
-                raise CensusAnalyserError("Check file path")
-        except ValueError:
-                raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
-
+            raise CensusAnalyserError("Wrong Delimiter or Invalid Columns Name")
 
 if __name__ == "__main__":
+
     x  = CSVLoader(path="C:/Users/PRACHITI PATIL/PycharmProjects/CensusAnalyzer/resources/IndiaStateCensusData.csv")
     length = x.record_counter()
     print(length)
-    len1 = x.load_State_Code()
-    print(len1)
+    CSV = CSVLoader(path="C:/Users/PRACHITI PATIL/PycharmProjects/CensusAnalyzer/resources/IndiaStateCensusData.csv", obj=IndianCensus())
+    print(CSV.record_counter())
+    CSV = CSVLoader(path="C:/Users/PRACHITI PATIL/PycharmProjects/CensusAnalyzer/resources/IndiaStateCode.csv",obj=StateCensusAnalyzer())
+    print(CSV.record_counter())
